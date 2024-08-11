6e60e5b06bdf6ed121903ec7b467f1cc924abaea
neresfabio <neresfjcomunic@gmail.com>
2024-08-11 01:50:27 -0300

    ➕ :heavy_plus_sign: Dependência Taillwind

--- client/buscafarma-app/src/components/header/Header.jsx
+++ client/buscafarma-app/src/components/header/Header.jsx
@@ -1,17 +1,16 @@
-import './Header.css';
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="logo">Logo</div>
-            <nav className="menu">
-                <ul>
-                    <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Ajuda</a></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="flex justify-between items-center px-6 h-16 bg-gray-100">
+      <div className="text-xl font-bold text-gray-800">Minha Logo</div>
+      <nav>
+        <ul className="flex space-x-6">
+          <li><a href="#home" className="text-gray-800 hover:text-blue-600">Início</a></li>
+          <li><a href="#about" className="text-gray-800 hover:text-blue-600">Sobre</a></li>
+          <li><a href="#contact" className="text-gray-800 hover:text-blue-600">Contato</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
